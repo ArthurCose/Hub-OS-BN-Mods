868ab6e337bad03c2be502a5da1f1a1cdfa424d9
Arthur Cosentino <arthurcose@gmail.com>
2024-04-01 18:04:11 -0400

    Update sword, wide sword, long sword

--- mods/cards/BN6_Standard_072/entry.lua
+++ mods/cards/BN6_Standard_072/entry.lua
@@ -1,8 +1,16 @@
+local bn_helpers = require("dev.GladeWoodsgrove.BattleNetworkHelpers")
+
+local SLASH_TEXTURE = bn_helpers.load_texture("sword_slashes.png")
+local BLADE_TEXTURE = bn_helpers.load_texture("sword_blades.png")
+
+local SLASH_ANIM_PATH = bn_helpers.fetch_animation_path("sword_slashes.animation")
+local BLADES_ANIM_PATH = bn_helpers.fetch_animation_path("sword_blades.animation")
+
+local AUDIO = bn_helpers.load_audio("sword.ogg")
+
 function card_init(actor, props)
 	local action = Action.new(actor, "PLAYER_SWORD")
 	action:set_lockout(ActionLockout.new_animation())
-	local SLASH_TEXTURE = Resources.load_texture("spell_sword_slashes.png")
-	local BLADE_TEXTURE = Resources.load_texture("spell_sword_blades.png")
 	action.on_execute_func = function(self, user)
 		self:add_anim_action(2,
 			function()
@@ -22,7 +30,7 @@ function card_init(actor, props)
 				blade_sprite:set_layer(-1)
 
 				local blade_anim = blade:animation()
-				blade_anim:load("spell_sword_blades.animation")
+				blade_anim:load(BLADES_ANIM_PATH)
 				blade_anim:set_state("DEFAULT")
 			end
 		)
@@ -33,8 +41,8 @@ function card_init(actor, props)
 			local fx = Artifact.new()
 			fx:set_facing(sword:facing())
 			local anim = fx:animation()
-			fx:set_texture(SLASH_TEXTURE, true)
-			anim:load("spell_sword_slashes.animation")
+			fx:set_texture(SLASH_TEXTURE)
+			anim:load(SLASH_ANIM_PATH)
 			anim:set_state("LONG")
 			anim:on_complete(function()
 				fx:erase()
@@ -82,7 +90,7 @@ function create_slash(user, props)
 	spell.can_move_to_func = function(tile)
 		return true
 	end
-	local AUDIO = Resources.load_audio("sfx.ogg")
+
 	Resources.play_audio(AUDIO)
 
 	return spell
