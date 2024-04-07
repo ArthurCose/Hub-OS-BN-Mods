5f5d7d84ede6798921c2bd54439bc2f1a2b960b7
Arthur Cosentino <arthurcose@gmail.com>
2024-04-07 12:39:30 -0400

    Move BN Assets lib to a shared namespace

--- mods/libraries/battle_network_assets/entry.lua
+++ mods/libraries/battle_network_assets/entry.lua
@@ -1,30 +1,30 @@
-local HelpersBN = {}
+local Lib = {}
 
 local images_animations_folder = _folder_path .. "Animations & Images/"
 local sounds_folder = _folder_path .. "Sounds/"
 
-function HelpersBN.fetch_animation_path(name)
+function Lib.fetch_animation_path(name)
     -- This is because we'll add the extension ourselves.
     return images_animations_folder .. name
 end
 
-function HelpersBN.load_texture(name)
+function Lib.load_texture(name)
     return Resources.load_texture(images_animations_folder .. name)
 end
 
-function HelpersBN.load_audio(name)
+function Lib.load_audio(name)
     return Resources.load_audio(sounds_folder .. name)
 end
 
 -- Poof support
-HelpersBN.ParticlePoof = {}
-function HelpersBN.ParticlePoof.new()
-    local TEXTURE = HelpersBN.load_texture("poof.png")
+Lib.ParticlePoof = {}
+function Lib.ParticlePoof.new()
+    local TEXTURE = Lib.load_texture("poof.png")
     local fx = Artifact.new()
     fx:set_texture(TEXTURE)
 
     local fx_animation = fx:animation()
-    fx_animation:load(HelpersBN.fetch_animation_path("poof.animation"))
+    fx_animation:load(Lib.fetch_animation_path("poof.animation"))
     fx_animation:set_state("DEFAULT")
     fx_animation:on_complete(function()
         fx:erase()
@@ -33,15 +33,15 @@ function HelpersBN.ParticlePoof.new()
     return fx;
 end
 
-HelpersBN.MobMove = {}
-function HelpersBN.MobMove.new(target)
+Lib.MobMove = {}
+function Lib.MobMove.new(target)
     local fx = Artifact.new()
     local field = target:field()
     local anim = fx:animation()
 
-    fx:set_texture(HelpersBN.load_texture("mob_move.png"))
+    fx:set_texture(Lib.load_texture("mob_move.png"))
 
-    anim:load(HelpersBN.fetch_animation_path("mob_move.animation"))
+    anim:load(Lib.fetch_animation_path("mob_move.animation"))
     anim:set_state("DEFAULT")
     anim:apply(fx:sprite())
     anim:on_complete(function()
@@ -51,4 +51,4 @@ function HelpersBN.MobMove.new(target)
     field:spawn(fx, target:current_tile())
 end
 
-return HelpersBN;
+return Lib;
