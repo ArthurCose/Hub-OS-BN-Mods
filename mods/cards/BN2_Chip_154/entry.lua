43e1ccf55a00e46e6f29ca8fe216746a6ac7e6ac
Arthur Cosentino <arthurcose@gmail.com>
2024-05-03 06:37:21 -0400

    Track StoneBod chip

---
+++ mods/cards/BN2_Chip_154/entry.lua
@@ -0,0 +1,12 @@
+function card_init(actor, props)
+    local action = Action.new(actor, "PLAYER_IDLE");
+    local frames = { { 1, 30 } }
+
+    action:override_animation_frames(frames);
+
+    action.on_execute_func = function(self, user)
+        user:apply_status(Hit.StoneBody, 1800)
+    end
+
+    return action;
+end
