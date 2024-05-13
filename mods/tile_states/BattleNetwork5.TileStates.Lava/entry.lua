91476cbcccd863388051a76ac36ff8f5f176f60e
Arthur Cosentino <arthurcose@gmail.com>
2024-05-13 09:09:29 -0400

    Improve lava tile state

--- mods/tile_states/BattleNetwork5.TileStates.Lava/entry.lua
+++ mods/tile_states/BattleNetwork5.TileStates.Lava/entry.lua
@@ -1,77 +1,43 @@
+local bn_assets = require("BattleNetwork.Assets")
+
+local HIT_TEXTURE = bn_assets.load_texture("bn6_hit_effects.png")
+local HIT_ANIMATION_PATH = bn_assets.fetch_animation_path("bn6_hit_effects.animation")
+
+local function try_hit(entity)
+  if not entity:hittable() or entity:intangible() or entity:ignoring_negative_tile_effects() or entity:element() == Element.Fire then
+    return
+  end
+
+  entity:hit(HitProps.new(50, Hit.Flinch | Hit.Impact, Element.Fire))
+
+  local tile = entity:current_tile()
+  tile:set_state(TileState.Normal)
+
+  local artifact = Artifact.new()
+  artifact:set_texture(HIT_TEXTURE)
+  artifact:sprite():set_layer(-5)
+
+  local animation = artifact:animation()
+  animation:load(HIT_ANIMATION_PATH)
+  animation:set_state("FIRE")
+  animation:on_complete(function()
+    artifact:erase()
+  end)
+
+  entity:field():spawn(artifact, tile)
+end
+
 ---@param custom_state CustomTileState
 function tile_state_init(custom_state)
-  local field = custom_state:field()
-  local tracked_auxprops = {}
+  custom_state.on_update_func = function(self, tile)
+    tile:find_characters(function(entity)
+      try_hit(entity)
+      return false
+    end)
 
-  custom_state.on_entity_enter_func = function(self, entity)
-    if not Character.from(entity) and not Obstacle.from(entity) then
-      return
-    end
-
-    if tracked_auxprops[entity:id()] then
-      -- already applied
-      return
-    end
-
-    local double_damage_prop = AuxProp.new()
-        :require_hit_element(Element.Aqua)
-        :increase_hit_damage("DAMAGE")
-    entity:add_aux_prop(double_damage_prop)
-
-    tracked_auxprops[entity:id()] = { double_damage_prop }
-
-    if entity:ignoring_negative_tile_effects() or entity:element() == Element.Fire then
-      return
-    end
-
-    entity:hit(HitProps.new(50, Hit.Flinch | Hit.Impact, Element.Fire))
-
-    local tile = entity:current_tile()
-    field:spawn(Explosion.new(), tile)
-    tile:set_state(TileState.Normal)
-  end
-
-  custom_state.on_entity_leave_func = function(self, entity)
-    local aux_props = tracked_auxprops[entity:id()]
-
-    if not aux_props then
-      return
-    end
-
-    if entity:current_tile():state() == TileState.Grass then
-      -- no need to remove aux props
-      return
-    end
-
-    for _, aux_prop in ipairs(aux_props) do
-      entity:remove_aux_prop(aux_prop)
-    end
-
-    tracked_auxprops[entity:id()] = nil
-  end
-
-  custom_state.change_request_func = function(self, tile)
-    for id in pairs(tracked_auxprops) do
-      local entity = field:get_entity(id)
-
-      if not entity then
-        tracked_auxprops[id] = nil
-        goto continue
-      end
-
-      if entity:current_tile() == tile then
-        local aux_props = tracked_auxprops[id]
-
-        for _, aux_prop in ipairs(aux_props) do
-          entity:remove_aux_prop(aux_prop)
-        end
-
-        tracked_auxprops[entity:id()] = nil
-      end
-
-      ::continue::
-    end
-
-    return true
+    tile:find_obstacles(function(entity)
+      try_hit(entity)
+      return false
+    end)
   end
 end
