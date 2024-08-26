152df6b3e969023b0d34a3c72668e26503eb58f2
Arthur Cosentino <arthurcose@gmail.com>
2024-08-26 06:47:50 -0400

    Update Bubble and WhiCapsule

--- mods/cards/BN6_Standard_192/entry.lua
+++ mods/cards/BN6_Standard_192/entry.lua
@@ -3,7 +3,7 @@ function card_mutate(player, index)
 
     if left_card and left_card.can_boost then
         -- update the left_card
-        left_card.hit_flags = left_card.hit_flags | Hit.Paralyze
+        left_card.hit_flags = left_card.hit_flags & ~Hit.mutual_exclusions_for(Hit.Paralyze) | Hit.Paralyze
         player:set_field_card(index - 1, left_card)
 
         -- remove this card
