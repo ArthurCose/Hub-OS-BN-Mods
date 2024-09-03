ab94272176ce5a7d32397934caf795e1638b68d3
Arthur Cosentino <arthurcose@gmail.com>
2024-09-03 13:55:18 -0400

    Track Giga Cannon PA

---
+++ mods/cards/BN6_ProgramAdvance_003/entry.lua
@@ -0,0 +1,14 @@
+function card_init(actor, props)
+	-- Almost identical to Cannon so reuse it
+	local card_properties = CardProperties.from_package("dev.GladeWoodsgrove.ZeroDamageCannon")
+
+	-- Change damage to what's taken in from toml + modifiers
+	card_properties.damage = props.damage
+
+	-- Change name as well
+	card_properties.short_name = props.short_name;
+
+	-- Change tags, of course
+	card_properties.tags = props.tags
+	return Action.from_card(actor, card_properties);
+end
