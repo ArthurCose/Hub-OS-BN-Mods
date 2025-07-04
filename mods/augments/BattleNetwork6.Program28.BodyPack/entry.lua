a669e938fe07019e7f22a6a2f2ce4e9dabd468ee
Arthur Cosentino <arthurcose@gmail.com>
2025-07-04 12:50:20 -0400

    Fix BodyPack using old ids

--- mods/augments/BattleNetwork6.Program28.BodyPack/entry.lua
+++ mods/augments/BattleNetwork6.Program28.BodyPack/entry.lua
@@ -3,5 +3,5 @@ function augment_init(augment)
     owner:boost_augment("BattleNetwork6.Program01.SuperArmor", 1)
     owner:boost_augment("BattleNetwork6.Program11.FloatShoes", 1)
     owner:boost_augment("BattleNetwork6.Program12.AirShoes", 1)
-    owner:boost_augment("BattleNetwork6.Program15.UnderShirt", 1)
+    owner:boost_augment("BattleNetwork6.Program13.UnderShirt", 1)
 end
