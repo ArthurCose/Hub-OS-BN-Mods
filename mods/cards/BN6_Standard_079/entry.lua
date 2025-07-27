fcdeb3e8d80030e343f39a5bd6de89d0e533e94f
Arthur Cosentino <arthurcose@gmail.com>
2025-07-27 15:01:29 -0400

    Use movement instead of drag for windrack gusts

--- mods/cards/BN6_Standard_079/entry.lua
+++ mods/cards/BN6_Standard_079/entry.lua
@@ -14,12 +14,12 @@ local AUDIO = bn_assets.load_audio("windrack.ogg")
 
 local function create_gust(team, direction)
 	local spell = Spell.new(team)
-	spell:set_hit_props(HitProps.new(0, Hit.Drag, Element.Wind, nil, Drag.new(direction, 1)))
-
+	spell:set_hit_props(HitProps.new(0, 0, Element.Wind))
 
 	local i = 0
 	spell.on_update_func = function()
 		local tile = spell:current_tile()
+		spell:attack_tile()
 
 		i = i + 1
 
@@ -38,14 +38,19 @@ local function create_gust(team, direction)
 			return
 		end
 
-		spell:attack_tile(tile)
-
 		local next_tile = tile:get_tile(direction, 1)
 
-		if not next_tile then
+		if not next_tile or next_tile:is_edge() then
 			spell:erase()
 			return
 		end
+
+		tile:find_characters(function(character)
+			if character:team() ~= team then
+				character:slide(next_tile, 4)
+			end
+			return false
+		end)
 
 		spell:slide(next_tile, 4)
 	end
@@ -54,77 +59,54 @@ local function create_gust(team, direction)
 end
 
 ---@param user Entity
-local function create_spell(spells, user, props, x_offset, y_offset)
-	local field = user:field()
-	local h_tile = user:get_tile(user:facing(), x_offset)
-
-	if not h_tile then
-		return
-	end
-
-	local tile = h_tile:get_tile(Direction.Down, y_offset)
-
-	if not tile then
-		return
-	end
-
+local function create_slash(user, hit_props)
 	local spell = Spell.new(user:team())
 	spell:set_facing(user:facing())
-	spell:set_hit_props(
-		HitProps.from_card(
-			props,
-			user:context(),
-			Drag.new(user:facing(), field:width())
-		)
-	)
+	spell:set_hit_props(hit_props)
+	spell:set_texture(SLASH_TEXTURE)
 
-	spell.on_update_func = function(self)
-		self:attack_tile()
-	end
-
-	field:spawn(spell, tile)
-
-	spells[#spells + 1] = spell
-end
-
----@param user Entity
-local function spawn_artifact(spells, user, state)
-	local fx = Artifact.new()
-	fx:set_facing(user:facing())
-	local anim = fx:animation()
-	fx:set_texture(SLASH_TEXTURE)
+	local anim = spell:animation()
 	anim:load(SLASH_ANIM_PATH)
-	anim:set_state(state)
+	anim:set_state("DEFAULT")
 	anim:on_complete(function()
-		fx:erase()
-
-		for _, spell in ipairs(spells) do
-			spell:delete()
-		end
+		spell:delete()
 	end)
 
-	local field = user:field()
-	local tile = user:get_tile(user:facing(), 1)
-
-	if tile then
-		field:spawn(fx, tile)
+	local attack_existing_tile = function(tile)
+		if tile then spell:attack_tile(tile) end
 	end
+
+	spell.on_update_func = function()
+		spell:attack_tile()
+		attack_existing_tile(spell:get_tile(Direction.Up, 1))
+		attack_existing_tile(spell:get_tile(Direction.Down, 1))
+	end
+
+	return spell
 end
 
 ---@param user Entity
 function card_init(user, props)
 	return sword:create_action(user, function()
-		local spells = {}
-		spawn_artifact(spells, user, "DEFAULT")
-		create_spell(spells, user, props, 1, -1)
-		create_spell(spells, user, props, 1, 0)
-		create_spell(spells, user, props, 1, 1)
+		local forward_tile = user:get_tile(user:facing(), 1)
+
+		if not forward_tile then
+			return
+		end
+
+		local field = user:field()
+		local hit_props = HitProps.from_card(
+			props,
+			user:context(),
+			Drag.new(user:facing(), field:width())
+		)
+
+		field:spawn(create_slash(user, hit_props), forward_tile)
 
 		local team = user:team()
-		local field = user:field()
-		local x = user:get_tile(user:facing(), 1):x()
+		local x = forward_tile:x()
 
-		for y = 0, field:height() do
+		for y = 0, field:height() - 1 do
 			field:spawn(create_gust(team, user:facing()), x, y)
 		end
 
