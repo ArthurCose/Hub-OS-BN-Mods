c0b9a8dca7408187dbf10db8469223e2b473aff1
Arthur Cosentino <arthurcose@gmail.com>
2025-07-27 13:16:55 -0400

    Control ignore holes from another augment

--- mods/augments/HubOS.Augments.IgnoreHoles/entry.lua
+++ mods/augments/HubOS.Augments.IgnoreHoles/entry.lua
@@ -2,9 +2,9 @@
 function augment_init(augment)
   local player = augment:owner()
 
-  player:boost_augment("HubOS.Augments.IgnoreHoles", 1)
+  player:ignore_hole_tiles(true)
 
   augment.on_delete_func = function()
-    player:boost_augment("HubOS.Augments.IgnoreHoles", -1)
+    player:ignore_hole_tiles(false)
   end
 end
