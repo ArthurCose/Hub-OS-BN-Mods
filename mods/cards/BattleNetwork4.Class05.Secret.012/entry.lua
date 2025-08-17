a7f017c7e0958789522f9b9efbf2249c3ce6655a
Arthur Cosentino <arthurcose@gmail.com>
2025-08-17 16:27:07 -0400

    Pull in new changes

--- mods/cards/BattleNetwork4.Class05.Secret.012/entry.lua
+++ mods/cards/BattleNetwork4.Class05.Secret.012/entry.lua
@@ -5,7 +5,7 @@ local BombLib = require("dev.konstinople.library.bomb")
 
 local bomb = BombLib.new_bomb()
 bomb:set_bomb_texture(Resources.load_texture("grenade.png"))
-bomb:set_bomb_animation_path(_folder_path .."bomb.lua")
+bomb:set_bomb_animation_path(_folder_path .."grenade.animation")
 bomb:set_bomb_shadow(bn_assets.load_texture("bomb_shadow.png"))
 bomb:set_execute_sfx(bn_assets.load_audio("lob_bomb.ogg"))
 bomb:enable_seeking()
