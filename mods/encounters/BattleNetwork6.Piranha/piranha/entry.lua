be87beac446a5c979d832c3075594e681901ace2
Arthur Cosentino <arthurcose@gmail.com>
2025-08-19 02:18:47 -0400

    Simplify Piranha

--- mods/encounters/BattleNetwork6.Piranha/piranha/entry.lua
+++ mods/encounters/BattleNetwork6.Piranha/piranha/entry.lua
@@ -29,6 +29,20 @@ local props_by_rank = {
     gape_duration = 44,
     cursor_frames_per_tile = 22
   },
+  [Rank.Rare1] = {
+    health = 140,
+    attack = 60,
+    idle_steps = 0,
+    gape_duration = 38,
+    cursor_frames_per_tile = 22
+  },
+  [Rank.Rare2] = {
+    health = 210,
+    attack = 100,
+    idle_steps = 0,
+    gape_duration = 32,
+    cursor_frames_per_tile = 21
+  },
 }
 
 local palettes_by_rank = {
@@ -36,11 +50,22 @@ local palettes_by_rank = {
   [Rank.V2] = "v2.palette.png",
   [Rank.V3] = "v3.palette.png",
   [Rank.SP] = "sp.palette.png",
+  [Rank.Rare1] = "rare1.palette.png",
+  [Rank.Rare2] = "rare2.palette.png",
 }
 
 ---@param character Entity
 function character_init(character)
-  character:set_name("Piranha")
-  character:set_palette(palettes_by_rank[character:rank()] or palettes_by_rank[Rank.V1])
-  shared_init(character, props_by_rank[character:rank()] or props_by_rank[Rank.V1])
+  local rank = character:rank()
+
+  if rank == Rank.Rare1 then
+    character:set_name("RarePira1")
+  elseif rank == Rank.Rare2 then
+    character:set_name("RarePira2")
+  else
+    character:set_name("Piranha")
+  end
+
+  character:set_palette(palettes_by_rank[rank] or palettes_by_rank[Rank.V1])
+  shared_init(character, props_by_rank[rank] or props_by_rank[Rank.V1])
 end
