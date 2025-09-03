3f839eecfbb339a3f469fd1be065fb878076b118
Arthur Cosentino <arthurcose@gmail.com>
2025-09-03 05:57:11 -0400

    Fix coming and going road

--- mods/cards/BN6_Standard_172/entry.lua
+++ mods/cards/BN6_Standard_172/entry.lua
@@ -1,68 +1,75 @@
-local AUDIO = Resources.load_audio("sfx.ogg")
-local FINISH_AUDIO = Resources.load_audio("finish_sfx.ogg")
+local bn_assets = require("BattleNetwork.Assets")
+local PANEL_CHANGE_SFX = bn_assets.load_audio("panel_change_indicate.ogg")
+local FINISH_AUDIO = bn_assets.load_audio("panel_change_finish.ogg")
 
-function card_init(actor, props)
-	local action = Action.new(actor, "CHARACTER_IDLE")
-
+---@param user Entity
+function card_init(user, props)
+	local action = Action.new(user, "CHARACTER_IDLE")
 	action:set_lockout(ActionLockout.new_sequence())
 
-	action.on_execute_func = function(self, user)
-		self.tile = actor:current_tile()
-		self.dir = actor:facing()
-		self.count = 0
-		self.max = 6
+	action.on_execute_func = function()
+		local step = action:create_step()
 
-		local step1 = self:create_step()
+		local start_tile = user:current_tile()
+		local facing = user:facing()
 
-		local ref = self
-		local tile = nil
+		-- resolve the target tile state based on the user's facing direction
+		local target_state = TileState.ConveyorLeft
+
+		if facing == Direction.Left then
+			target_state = TileState.ConveyorRight
+		end
+
+		---@type table<Tile, TileState>
+		local prev_states = {}
 		local k = 0
 		local cooldown = 0
-		local previous_state = nil
-		step1.on_update_func = function(self)
-			if cooldown <= 0 then
-				k = k + 1
-				cooldown = 5
-				Resources.play_audio(AUDIO)
-				for i = ref.count, ref.max, 1 do
-					tile = ref.tile:get_tile(ref.dir, i)
-					if tile and tile:state() ~= TileState.ConveyorLeft and tile:state() ~= TileState.ConveyorRight then
-						previous_state = tile:state()
-					end
-					if tile and tile:team() ~= ref.tile:team() and not tile:is_edge() then
-						if ref.dir == Direction.Left then
-							if tile:state() == TileState.ConveyorRight and previous_state ~= nil then
-								tile:set_state(previous_state)
-							else
-								tile:set_state(TileState.ConveyorRight)
-							end
-						else
-							if tile:state() == TileState.ConveyorLeft and previous_state ~= nil then
-								tile:set_state(previous_state)
-							else
-								tile:set_state(TileState.ConveyorLeft)
-							end
-						end
-					end
-				end
-			else
+
+		step.on_update_func = function()
+			if cooldown > 0 then
 				cooldown = cooldown - 1
+				return
 			end
-			if k == 14 then
-				Resources.play_audio(FINISH_AUDIO)
-				self:complete_step()
-			end
-		end
-		for i = ref.count, ref.max, 1 do
-			tile = ref.tile:get_tile(ref.dir, i)
-			if tile and tile:team() ~= ref.tile:team() and not tile:is_edge() then
-				if ref.dir == Direction.Left then
-					tile:set_state(TileState.ConveyorRight)
-				else
-					tile:set_state(TileState.ConveyorLeft)
+
+			k = k + 1
+			cooldown = 3
+
+			for i = 0, Field.width(), 1 do
+				local tile = start_tile:get_tile(facing, i)
+
+				if not tile or tile:is_edge() then
+					break
 				end
+
+				if tile:team() == user:team() then
+					goto continue
+				end
+
+				if tile:state() ~= target_state then
+					prev_states[tile] = tile:state()
+				end
+
+				local prev_state = prev_states[tile]
+
+				if tile:state() == target_state and prev_state ~= nil then
+					tile:set_state(prev_state)
+				else
+					tile:set_state(target_state)
+				end
+
+				::continue::
+			end
+
+			if k == 17 then
+				Resources.play_audio(PANEL_CHANGE_SFX, AudioBehavior.EndLoop)
+				Resources.play_audio(FINISH_AUDIO)
+				step:complete_step()
 			end
 		end
+
+		local SAMPLE_RATE = 44100
+		Resources.play_audio(PANEL_CHANGE_SFX, AudioBehavior.LoopSection(0, SAMPLE_RATE / 60 * 4))
 	end
+
 	return action
 end
