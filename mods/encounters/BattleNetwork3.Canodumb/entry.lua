2983d1fb863d7c1d24d777c7745bc932491c8d3f
Arthur Cosentino <arthurcose@gmail.com>
2025-09-08 19:17:53 -0400

    Add type hint to canodumb's encounter entry file

--- mods/encounters/BattleNetwork3.Canodumb/entry.lua
+++ mods/encounters/BattleNetwork3.Canodumb/entry.lua
@@ -1,5 +1,6 @@
 local character_id = "BattleNetwork3.Canodumb.Enemy"
 
+---@param encounter Encounter
 function encounter_init(encounter)
     encounter
         :create_spawner(character_id, Rank.V1)
