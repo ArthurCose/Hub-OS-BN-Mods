ea05c20741e6c71d9719baec042b0a045ca0daf2
Arthur Cosentino <arthurcose@gmail.com>
2025-09-15 10:59:41 -0400

    Update mod folders to match package id

---
+++ utils/update_folder_names/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+const toml = require("toml");
+
+const modsFolder = "../../mods";
+
+for (const categoryFolder of fs.readdirSync(modsFolder)) {
+  const categoryPath = path.join(modsFolder, categoryFolder);
+
+  for (const modFolder of fs.readdirSync(categoryPath)) {
+    if (modFolder.startsWith("_")) {
+      continue;
+    }
+
+    const modPath = path.join(categoryPath, modFolder);
+
+    try {
+      const contents = fs.readFileSync(
+        path.join(modPath, "package.toml"),
+        "utf8"
+      );
+      const packageMeta = toml.parse(contents);
+      const packageId = packageMeta.package.id;
+
+      if (modFolder == packageId) {
+        continue;
+      }
+
+      fs.renameSync(modPath, path.join(categoryPath, packageId));
+    } catch (err) {
+      console.err(`Failed to read package.toml in ${modPath}: \n`, err);
+    }
+  }
+}
