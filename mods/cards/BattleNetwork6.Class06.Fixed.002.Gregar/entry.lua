6c7eb1b4b565574befacf32f6e86d9c0b6b2cace
Arthur Cosentino <arthurcose@gmail.com>
2025-09-18 13:20:11 -0400

    AddElecMan

---
+++ mods/cards/BattleNetwork6.Class06.Fixed.002.Gregar/entry.lua
@@ -0,0 +1,187 @@
+---@type BattleNetwork.Assets
+local bn_assets = require("BattleNetwork.Assets")
+---@type dev.konstinople.library.sword
+local SwordLib = require("dev.konstinople.library.sword")
+
+local sword = SwordLib.new_sword()
+sword:set_blade_animation_state("ELEC")
+sword:set_blade_texture(bn_assets.load_texture("sword_blades.png"))
+sword:set_blade_animation_path(bn_assets.fetch_animation_path("sword_blades.animation"))
+sword:set_frame_data({ { 2, 0 }, { 2, 2 }, { 3, 2 }, { 4, 10 } })
+
+local SLASH_TEXTURE = bn_assets.load_texture("sword_slashes.png")
+local SLASH_ANIM_PATH = bn_assets.fetch_animation_path("sword_slashes.animation")
+local AUDIO = bn_assets.load_audio("sword.ogg")
+
+---@param user Entity
+local function create_spell(spells, user, props, x_offset, y_offset)
+  local h_tile = user:get_tile(user:facing(), x_offset)
+  if not h_tile then return end
+  local tile = h_tile:get_tile(Direction.Down, y_offset)
+
+  if not tile then
+    return
+  end
+
+  local spell = Spell.new(user:team())
+  spell:set_facing(user:facing())
+  spell:set_hit_props(
+    HitProps.from_card(
+      props,
+      user:context(),
+      Drag.None
+    )
+  )
+
+  spell.on_update_func = function(self)
+    self:current_tile():attack_entities(self)
+  end
+
+  Field.spawn(spell, tile)
+
+  spells[#spells + 1] = spell
+end
+
+---@param user Entity
+local function spawn_artifact(spells, user, state)
+  local tile = user:get_tile(user:facing(), 1)
+  if not tile then return end
+
+  -- using spell to avoid weird time freeze quirks
+  local fx = Spell.new()
+  fx:set_facing(user:facing())
+  local anim = fx:animation()
+  fx:set_texture(SLASH_TEXTURE)
+  anim:load(SLASH_ANIM_PATH)
+  anim:set_state(state)
+  anim:on_complete(function()
+    fx:erase()
+
+    for _, spell in ipairs(spells) do
+      spell:delete()
+    end
+  end)
+
+  Field.spawn(fx, tile)
+end
+
+
+---@param team Team
+---@param tile Tile?
+local function tile_contains_enemy(team, tile)
+  if not tile then
+    return false
+  end
+
+  local has_enemy = false
+  tile:find_characters(function(c)
+    if c:hittable() and c:team() ~= team then
+      has_enemy = true
+    end
+
+    return false
+  end)
+
+  return has_enemy
+end
+
+---@param user Entity
+---@param props CardProperties
+function card_init(user, props)
+  local action = Action.new(user, "ELEC_SWORD_GLIDE")
+  action:set_lockout(ActionLockout.new_sequence())
+
+  ---@type Tile?
+  local original_tile
+
+  action.on_execute_func = function()
+    original_tile = user:current_tile()
+    original_tile:reserve_for(user)
+  end
+
+  action.can_move_to_func = function(tile)
+    return not tile:is_edge() and not tile:is_reserved()
+  end
+
+  local glide_step = action:create_step()
+  glide_step.on_update_func = function()
+    local team = user:team()
+    local facing = user:facing()
+    local tile_ahead = user:get_tile(facing, 1)
+
+    local enemy_ahead =
+        tile_contains_enemy(team, tile_ahead) or
+        tile_contains_enemy(team, user:get_tile(Direction.join(facing, Direction.Up), 1)) or
+        tile_contains_enemy(team, user:get_tile(Direction.join(facing, Direction.Down), 1))
+
+    if enemy_ahead then
+      -- stop even if we're mid movement
+      local movement_offset = user:movement_offset()
+      user:set_offset(movement_offset.x, movement_offset.y)
+      user:cancel_movement()
+      user:set_movement_offset(0, 0)
+
+      user:set_counterable(true)
+      glide_step:complete_step()
+      return
+    end
+
+    if user:is_moving() then
+      return
+    end
+
+    if not tile_ahead or not user:can_move_to(tile_ahead) then
+      glide_step:complete_step()
+      user:set_counterable(true)
+      return
+    end
+
+    user:slide(tile_ahead, 6)
+  end
+
+  sword:create_action_step(action, function()
+    local spells = {}
+    spawn_artifact(spells, user, "ELEC_WIDE")
+    create_spell(spells, user, props, 1, -1)
+    create_spell(spells, user, props, 1, 0)
+    create_spell(spells, user, props, 1, 1)
+
+    Resources.play_audio(AUDIO)
+    user:set_counterable(false)
+  end)
+
+  local wait_time = 0
+  local return_and_wait_step = action:create_step()
+  return_and_wait_step.on_update_func = function()
+    if wait_time == 0 and original_tile then
+      original_tile:add_entity(user)
+      original_tile:remove_reservation_for(user)
+      original_tile = nil
+
+      local animation = user:animation()
+      animation:set_state("CHARACTER_MOVE")
+      animation:set_playback(Playback.Reverse)
+
+      user:set_offset(0, 0)
+    end
+
+    wait_time = wait_time + 1
+
+    if wait_time >= 35 then
+      return_and_wait_step:complete_step()
+    else
+    end
+  end
+
+  action.on_action_end_func = function()
+    if original_tile then
+      original_tile:remove_reservation_for(user)
+      original_tile:add_entity(user)
+    end
+
+    user:set_offset(0, 0)
+    user:set_counterable(false)
+  end
+
+  return action
+end
---
+++ mods/players/dev.konstinople.player.ElecMan/entry.lua
@@ -0,0 +1,340 @@
+---@type BattleNetwork.Assets
+local bn_assets = require("BattleNetwork.Assets")
+---@type BattleNetwork.Emotions
+local EmotionsLib = require("BattleNetwork.Emotions")
+---@type dev.konstinople.library.bomb
+local BombLib = require("dev.konstinople.library.bomb")
+
+local THUNDERBOLT_TEXTURE = bn_assets.load_texture("thunderbolt.png")
+local THUNDERBOLT_ANIMATION_PATH = bn_assets.fetch_animation_path("thunderbolt.animation")
+local THUNDERBOLT_SFX = bn_assets.load_audio("dollthunder.ogg")
+
+local ROD_TEXTURE = Resources.load_texture("elec_rod.png")
+local ROD_ANIMATION_PATH = _folder_path .. "elec_rod.animation"
+local ROD_SHADOW = bn_assets.load_texture("bomb_shadow.png")
+
+local EXPLOSION_TEXTURE = bn_assets.load_texture("spell_explosion.png")
+local EXPLOSION_ANIM_PATH = bn_assets.fetch_animation_path("spell_explosion.animation")
+local EXPLOSION_SFX = bn_assets.load_audio("explosion_defeatedmob.ogg")
+
+local FIXED_CARD_ID = "BattleNetwork6.Class06.Fixed.002.Gregar"
+
+local rod_bomb = BombLib.new_bomb()
+rod_bomb:set_bomb_texture(ROD_TEXTURE)
+rod_bomb:set_bomb_animation_path(ROD_ANIMATION_PATH)
+rod_bomb:set_bomb_shadow(ROD_SHADOW)
+rod_bomb:set_execute_sfx(bn_assets.load_audio("lob_bomb.ogg"))
+
+local function spawn_hit_artifact(entity)
+  local movement_offset = entity:movement_offset()
+
+  local particle = bn_assets.HitParticle.new("ELEC")
+
+  particle:set_offset(
+    movement_offset.x + math.random(-8, 8),
+    movement_offset.y - entity:height() // 2 + math.random(-8, 8)
+  )
+
+  Field.spawn(particle, entity:current_tile())
+end
+
+---@param player Entity
+local function spawn_h_thunderbolt(player)
+  local tile = player:current_tile()
+  local facing = player:facing()
+  local spell_offset = player:animation():relative_point("POINTER")
+
+  local spell = Spell.new(player:team())
+  spell:set_offset(spell_offset.x, spell_offset.y)
+  spell:set_facing(facing)
+  spell:set_layer(1)
+  spell:set_texture(THUNDERBOLT_TEXTURE)
+
+  local spell_anim = spell:animation()
+  spell_anim:load(THUNDERBOLT_ANIMATION_PATH)
+  spell_anim:set_state("DEFAULT")
+  spell_anim:set_playback(Playback.Loop)
+
+  spell:set_hit_props(HitProps.new(
+    20 * player:attack_level() + 40,
+    Hit.Flinch | Hit.Flash,
+    Element.Elec,
+    player:context()
+  ))
+
+  local attack_list = { tile }
+
+  for i = 1, 5 do
+    local new_tile = tile:get_tile(facing, i)
+    if new_tile then
+      attack_list[#attack_list + 1] = new_tile
+    end
+  end
+
+  local time = 0
+
+  spell.on_update_func = function()
+    for _, tile in ipairs(attack_list) do
+      spell:attack_tile(tile)
+    end
+
+    time = time + 1
+
+    if time >= 18 then
+      spell:delete()
+    end
+  end
+
+  spell.on_spawn_func = function()
+    Resources.play_audio(THUNDERBOLT_SFX)
+  end
+
+  spell.on_attack_func = function(_, other)
+    spawn_hit_artifact(other)
+  end
+
+  Field.spawn(spell, tile)
+end
+
+local function create_elec_rod()
+  local rod = Obstacle.new(Team.Other)
+  rod:set_health(40)
+  rod:set_never_flip(true)
+  rod:set_texture(ROD_TEXTURE)
+  rod:set_shadow(ROD_SHADOW)
+
+  rod:set_hit_props(
+    HitProps.new(
+      50,
+      Hit.Paralyze | Hit.Flinch | Hit.Flash,
+      Element.Elec
+    )
+  )
+
+  rod:add_aux_prop(AuxProp.new():declare_immunity(~Hit.None))
+
+  local animation = rod:animation()
+  animation:load(ROD_ANIMATION_PATH)
+  animation:set_state("DEFAULT")
+
+  local defense_rule = DefenseRule.new(DefensePriority.Body, DefenseOrder.Always)
+  local attacking = false
+
+  defense_rule.defense_func = function(defense, _, _, hit_props)
+    if hit_props.flags & Hit.Drain ~= 0 then
+      return
+    end
+
+    if hit_props.element == Element.Break or hit_props.secondary_element == Element.Break or hit_props.flags & Hit.PierceGuard ~= 0 then
+      rod:set_health(0)
+      return
+    end
+
+    if hit_props.element ~= Element.Elec and hit_props.secondary_element ~= Element.Elec then
+      return
+    end
+
+    defense:block_damage()
+
+    if attacking then
+      return
+    end
+
+    attacking = true
+
+    animation:set_state("SHOCK")
+    animation:on_complete(function()
+      attacking = false
+      animation:set_state("SPAWN")
+    end)
+  end
+
+  rod:add_defense_rule(defense_rule)
+
+  rod.on_update_func = function()
+    if not attacking then
+      -- attack only the current tile
+      rod:attack_tile()
+      return
+    end
+
+    local center_tile = rod:current_tile()
+    local center_x = center_tile:x()
+    local center_y = center_tile:y()
+
+    for y = center_y - 2, center_y + 2 do
+      rod:attack_tile(Field.tile_at(center_x, y))
+    end
+  end
+
+  rod.on_collision_func = function()
+    if not attacking then
+      -- explode if we weren't trying to attack,
+      -- as this was caused by an entity in the same tile as us
+      rod:set_health(0)
+    end
+  end
+
+  rod.on_attack_func = function(_, other)
+    spawn_hit_artifact(other)
+  end
+
+  rod.on_delete_func = function()
+    if rod:health() == 0 then
+      rod:erase()
+
+      local explosion = Artifact.new()
+      explosion:set_texture(EXPLOSION_TEXTURE)
+
+      local explosion_anim = explosion:animation()
+      explosion_anim:load(EXPLOSION_ANIM_PATH)
+      explosion_anim:set_state("DEFAULT")
+      explosion_anim:on_complete(function()
+        explosion:delete()
+      end)
+
+      Resources.play_audio(EXPLOSION_SFX)
+      Field.spawn(explosion, rod:current_tile())
+    else
+      animation:set_state("SPAWN")
+      animation:set_playback(Playback.Reverse)
+      animation:on_complete(function()
+        rod:erase()
+      end)
+    end
+  end
+
+  return rod
+end
+
+---@param player Entity
+function player_init(player)
+  player:set_height(60.0)
+  player:load_animation("battle.animation")
+  player:set_texture(Resources.load_texture("battle.png"))
+  player:ignoring_negative_tile_effects()
+
+  player:set_charge_position(2, -38)
+
+  local synchro = EmotionsLib.new_synchro()
+  synchro:implement(player)
+
+  -- chip boost
+  player:add_aux_prop(
+    AuxProp.new()
+    :require_card_time_freeze(false)
+    :require_card_element(Element.Elec)
+    :increase_card_damage(50)
+  )
+
+  player.normal_attack_func = function(self)
+    return Buster.new(self, false, player:attack_level())
+  end
+
+  local charge_timing = { 90, 80, 70, 65, 60 }
+  player.calculate_charge_time_func = function()
+    return charge_timing[player:charge_level()] or charge_timing[#charge_timing]
+  end
+
+  player.charged_attack_func = function()
+    local action = Action.new(player, "H_THUNDERBOLT_START")
+    action:set_lockout(ActionLockout.new_sequence())
+    action:create_step()
+
+    action.on_execute_func = function()
+      local animation = player:animation()
+      animation:set_playback(Playback.Loop)
+
+      player:set_counterable(true)
+
+      local i = 0
+
+      animation:on_complete(function()
+        i = i + 1
+
+        if i < 3 then return end
+
+        i = 0
+
+        player:set_counterable(false)
+
+        animation:set_state("H_THUNDERBOLT_LOOP")
+        animation:set_playback(Playback.Loop)
+        animation:on_complete(function()
+          i = i + 1
+
+          if i == 4 then
+            action:end_action()
+          end
+        end)
+
+        spawn_h_thunderbolt(player)
+      end)
+    end
+
+    action.on_action_end_func = function()
+      player:set_counterable(false)
+    end
+
+    return action
+  end
+
+  ---@type Entity?
+  local prev_rod
+  local rod_cooldown = 0
+
+  player:create_component(Lifetime.ActiveBattle).on_update_func = function()
+    if rod_cooldown > 0 then
+      rod_cooldown = rod_cooldown - 1
+    end
+  end
+
+  player.special_attack_func = function()
+    if rod_cooldown > 0 then
+      return
+    end
+
+    rod_cooldown = 60 * 8
+
+    if prev_rod and not prev_rod:deleted() then
+      prev_rod:delete()
+    end
+
+    return rod_bomb:create_action(player, function(tile)
+      if tile then
+        local rod = create_elec_rod()
+        rod:set_owner(player)
+        Field.spawn(rod, tile)
+
+        prev_rod = rod
+      end
+    end)
+  end
+
+  -- fixed card
+  local card = CardProperties.from_package(FIXED_CARD_ID, "E")
+  card.damage = 40 + player:attack_level() * 20
+  local button = player:set_fixed_card(card)
+
+  local component = player:create_component(Lifetime.CardSelectOpen)
+
+  local prev_attack_level = player:attack_level()
+  component.on_update_func = function()
+    if button:deleted() then
+      component:eject()
+      return
+    end
+
+    local attack_level = player:attack_level()
+
+    if attack_level == prev_attack_level then
+      return
+    end
+
+    button:delete()
+
+    prev_attack_level = attack_level
+    card.damage = 40 + player:attack_level() * 20
+    button = player:set_fixed_card(card)
+  end
+end
