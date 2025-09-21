6d128d7de5a3e006faf0aaa43702178709e7a6a2
Arthur Cosentino <arthurcose@gmail.com>
2025-09-21 15:41:12 -0400

    Pull in lava change

--- mods/tile_states/BattleNetwork5.TileStates.Lava/entry.lua
+++ mods/tile_states/BattleNetwork5.TileStates.Lava/entry.lua
@@ -4,27 +4,32 @@ local HIT_TEXTURE = bn_assets.load_texture("bn6_hit_effects.png")
 local HIT_ANIMATION_PATH = bn_assets.fetch_animation_path("bn6_hit_effects.animation")
 
 local function try_hit(entity)
-  if not entity:hittable() or entity:intangible() or entity:ignoring_negative_tile_effects() or entity:element() == Element.Fire then
-    return
-  end
+  if not entity:hittable() then return end
+  if entity:intangible() then return end
+  if entity:ignoring_negative_tile_effects() then return end
+  if entity:element() == Element.Fire then return end
 
-  entity:hit(HitProps.new(50, Hit.Flinch, Element.Fire))
+  local props = HitProps.new(50, Hit.Flinch, Element.Fire)
 
   local tile = entity:current_tile()
-  tile:set_state(TileState.Normal)
 
-  local artifact = Artifact.new()
-  artifact:set_texture(HIT_TEXTURE)
-  artifact:sprite():set_layer(-5)
+  local spell = Spell.new(Team.Other)
 
-  local animation = artifact:animation()
-  animation:load(HIT_ANIMATION_PATH)
-  animation:set_state("FIRE")
-  animation:on_complete(function()
-    artifact:erase()
-  end)
+  spell:set_hit_props(props)
 
-  Field.spawn(artifact, tile)
+  spell.on_update_func = function(self)
+    self:attack_tile()
+  end
+
+  spell.on_collision_func = function(self, other)
+    local hit_fx = bn_assets.HitParticle.new("FIRE", math.random(-10, 10), math.random(-10, 10))
+
+    Field.spawn(hit_fx, tile)
+
+    tile:set_state(TileState.Normal)
+  end
+
+  Field.spawn(spell, tile)
 end
 
 ---@param custom_state CustomTileState
