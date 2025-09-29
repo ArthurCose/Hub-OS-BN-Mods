ddbfd4aae298994f6910e9713ce7630bec5d4c43
Arthur Cosentino <arthurcose@gmail.com>
2025-09-28 22:22:23 -0400

    Fix guardian animations

--- mods/cards/BattleNetwork6.Class01.Standard.154.Guardian/entry.lua
+++ mods/cards/BattleNetwork6.Class01.Standard.154.Guardian/entry.lua
@@ -3,12 +3,12 @@ local bn_assets = require("BattleNetwork.Assets")
 
 local texture = bn_assets.load_texture("guardian.png")
 local anim_path = bn_assets.fetch_animation_path("guardian.animation")
+local GUARDIAN_HEIGHT = 31
 
 local thunder_texture = bn_assets.load_texture("bn3_lightning.png")
 local thunder_anim_path = bn_assets.fetch_animation_path("bn3_lightning.animation")
 
 local audio = bn_assets.load_audio("elementman_thunder.ogg")
-local hit_audio = bn_assets.load_audio("hit_impact.ogg")
 
 local function punish(user, team)
     local action = Action.new(user, "ATTACK")
@@ -18,6 +18,7 @@ local function punish(user, team)
     local props = CardProperties.new()
 
     props.short_name = "Punishment"
+    props.damage = 200
     props.time_freeze = true
     props.prevent_time_freeze_counter = true
 
@@ -26,7 +27,6 @@ local function punish(user, team)
     local step = action:create_step()
 
     local time = 0
-    local start_timer = false
 
     action:add_anim_action(4, function()
         local enemy_list = Field.find_characters(function(ch)
@@ -63,27 +63,28 @@ local function punish(user, team)
             Field.spawn(spell, enemy_list[i]:current_tile())
         end
 
-        Field.shake(8, 30)
+        Field.shake(5, 60)
 
         Resources.play_audio(audio)
     end)
 
     action.on_animation_end_func = function()
-        start_timer = true
+        local artifact = bn_assets.MobMove.new("MEDIUM_END")
+        artifact:set_offset(0, -GUARDIAN_HEIGHT // 2)
+
+        local i = 0
+        artifact.on_update_func = function()
+            i = i + 1
+
+            if i == 2 then
+                user:hide()
+            end
+        end
+
+        Field.spawn(artifact, user:current_tile())
     end
 
     step.on_update_func = function()
-        if start_timer == false then return end
-
-        if time == 0 then user:animation():set_state("HIT") end
-
-        if time == 50 then
-            local movement = bn_assets.MobMove.new("MEDIUM_START")
-            movement:animation():on_frame(2, function()
-                user:hide()
-            end)
-        end
-
         time = time + 1
 
         if time == 60 then
@@ -106,17 +107,19 @@ function card_init(user)
     action:set_lockout(ActionLockout.new_sequence())
 
     action.on_execute_func = function()
-        local mob_move = bn_assets.MobMove.new("MEDIUM_END")
-
         local tile = user:get_tile(user:facing(), 1)
-        mob_move:animation():on_frame(2, function()
+        if not tile then return end
+
+        local artifact = bn_assets.MobMove.new("MEDIUM_START")
+        artifact:set_offset(0, -GUARDIAN_HEIGHT // 2)
+
+        artifact.on_spawn_func = function()
             if not tile:is_walkable() then return end
 
             local guardian = Obstacle.new(Team.Other)
-
             guardian:set_health(2048)
+            guardian:set_height(GUARDIAN_HEIGHT)
             guardian:set_owner(user:team())
-            guardian:enable_hitbox(true)
             guardian:add_aux_prop(AuxProp.new():declare_immunity(~Hit.Impact))
             guardian:set_texture(texture)
 
@@ -124,27 +127,32 @@ function card_init(user)
             guardian_anim:load(anim_path)
             guardian_anim:set_state("IDLE")
 
+            local activated = false
             local punishment_defense = DefenseRule.new(DefensePriority.Body, DefenseOrder.Always)
 
             punishment_defense.defense_func = function(defense, attacker, defender, hit_props)
                 if hit_props.flags & Hit.Drain ~= 0 then return end
                 if hit_props.damage == 0 then return end
 
+                if activated then
+                    return
+                end
+
                 punish(guardian, attacker:team())
+                activated = true
+            end
 
-                defense:block_damage()
-
-                Resources.play_audio(hit_audio)
-
-                guardian:remove_defense_rule(punishment_defense)
+            punishment_defense.filter_func = function(hit_props)
+                hit_props.damage = 1
+                return hit_props
             end
 
             guardian:add_defense_rule(punishment_defense)
 
             Field.spawn(guardian, tile)
-        end)
+        end
 
-        Field.spawn(mob_move, tile)
+        Field.spawn(artifact, tile)
     end
 
     return action
