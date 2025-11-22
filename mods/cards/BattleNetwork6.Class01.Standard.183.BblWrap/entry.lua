85977ade92a3d1c2fa74fc8e14af2a1999f3bca4
Arthur Cosentino <arthurcose@gmail.com>
2025-11-22 12:10:32 -0500

    Pull in BblWrap

---
+++ mods/cards/BattleNetwork6.Class01.Standard.183.BblWrap/entry.lua
@@ -0,0 +1,75 @@
+local bn_assets = require("BattleNetwork.Assets")
+
+local BarrierLib = require("dev.GladeWoodsgrove.library.BarriersAndAuras")
+
+local BARRIER_TEXTURE = bn_assets.load_texture("bubble.png")
+local BARRIER_ANIMATION_PATH = bn_assets.fetch_animation_path("bubble.animation")
+local BARRIER_UP_SOUND = bn_assets.load_audio("bblwrap_pop.ogg")
+local DESTRUCTION_SOUND = bn_assets.load_audio("bubble_pop.ogg")
+
+function card_init(user, props)
+	local action = Action.new(user)
+	local step = action:create_step()
+	action:set_lockout(ActionLockout.new_sequence())
+
+	-- Inputs are (owner, health)
+	local barrier = BarrierLib.new_barrier(user, 1)
+
+	-- Barrier/Aura data
+	-- Texture & Animiation Path
+	barrier:set_texture(BARRIER_TEXTURE)
+	barrier:set_animation_path(BARRIER_ANIMATION_PATH)
+
+	-- Animation State that loops while active
+	barrier:set_active_state("WRAP")
+
+	-- Animation State that plays once when disappearing.
+	barrier:set_destroyed_state("WRAP_POP")
+
+	-- Sound to play when barrier health drops to zero.
+	barrier:set_destruction_sound(DESTRUCTION_SOUND)
+
+	-- Mechnical data
+	-- Set element that destroys the defense on hit.
+	barrier:set_weakness_element({ Element.Elec, Element.Wind })
+
+	barrier:set_regeneration_timer(283)
+	barrier:set_regeneration_audio(BARRIER_UP_SOUND)
+
+	local timer = 0
+	step.on_update_func = function(self)
+		timer = timer + 1
+		if timer < 60 then return end
+		self:complete_step()
+	end
+
+	local offset = { x = 0, y = -math.ceil(user:height() / 2) }
+
+	action.on_execute_func = function(self)
+		-- Adds the barrier to the user, completing the process.
+		barrier:add_to_owner(offset)
+
+		local elec_weakness = AuxProp.new()
+			:require_hit_flags_absent(Hit.Drain)
+			:require_hit_element(Element.Elec)
+			:increase_hit_damage("DAMAGE")
+			:with_callback(function()
+				local alert_artifact = Alert.new()
+				alert_artifact:sprite():set_never_flip(true)
+
+				local movement_offset = user:movement_offset()
+				alert_artifact:set_offset(movement_offset.x, movement_offset.y - user:height())
+
+				Field.spawn(alert_artifact, user:current_tile())
+				barrier:remove_barrier()
+			end)
+			:once()
+
+		user:add_aux_prop(elec_weakness)
+
+		-- Play the audio
+		Resources.play_audio(BARRIER_UP_SOUND)
+	end
+
+	return action
+end
---
+++ mods/libraries/dev.GladeWoodsgrove.library.BarriersAndAuras/entry.lua
@@ -0,0 +1,460 @@
+---@class BarrierLib
+local BarrierLib = {}
+BarrierLib.__index = BarrierLib
+
+local function debug_print(args)
+  print("[BARRIER LIBRARY]" .. args)
+end
+
+function BarrierLib:check_anim_exists(is_print_warning)
+  if self._anim_path == nil then
+    if is_print_warning == true then debug_print("Attempted to set animation behavior,\nbut no animation file was set!") end
+    return false
+  end
+  return true
+end
+
+function BarrierLib:set_display_health(is_draw)
+  self._is_draw_health = is_draw
+end
+
+-- Image that is added to the player when the defense is active.
+-- If unset, there will be no image, but it will print a debug warning.
+-- Use textures loaded by Resources.load_texture for better performance.
+function BarrierLib:set_texture(texture)
+  self._texture = texture
+end
+
+function BarrierLib:get_texture()
+  return self._texture
+end
+
+-- Animation file that will be used to animate the barrier.
+-- If unset, the barrier will remain static.
+-- This will draw the entire sprite sheet, and so this will print a debug warning just in case.
+function BarrierLib:set_animation_path(anim_path)
+  self._anim_path = anim_path
+end
+
+function BarrierLib:get_animation_path()
+  return self._anim_path
+end
+
+-- Sets the active state of the barrier
+-- This is the default state for when the barrier is active.
+function BarrierLib:set_active_state(state)
+  if self:check_anim_exists(true) == false then return end
+  self._active_state = state
+end
+
+function BarrierLib:get_active_state()
+  return self._active_state
+end
+
+-- Set the state for the barrier fading naturally
+-- Many barriers last a set amount of time, and will fade away if not destroyed by then.
+-- Most barriers share this state with their "Destroyed" behavior.
+function BarrierLib:set_fade_state(state)
+  if self:check_anim_exists(true) == false then return end
+  self._fade_state = state
+end
+
+function BarrierLib:set_destroyed_state(state)
+  if self:check_anim_exists(true) == false then return end
+  self._destroyed_state = state
+end
+
+function BarrierLib:set_destruction_sound(sound)
+  self._destruction_sound = sound
+end
+
+function BarrierLib:get_destruction_sound()
+  return self._destruction_sound
+end
+
+function BarrierLib:get_fade_state()
+  return self._fade_state
+end
+
+function BarrierLib:get_destroyed_state()
+  return self._destroyed_state
+end
+
+function BarrierLib:set_max_health(health)
+  self._max_health = health
+end
+
+function BarrierLib:set_health(health)
+  self._health = health
+end
+
+function BarrierLib:set_removal_timer(time)
+  self._removal_timer = time
+end
+
+function BarrierLib:get_health()
+  return self._health
+end
+
+function BarrierLib:get_max_health()
+  return self._max_health
+end
+
+-- Sets whether you are creating a Barrier-type or an Aura-type.
+-- Barrier-types are the default, and lose health on every hit. They are destroyed at 0 HP.
+-- Aura-types must be enabled, and only take damage if the hit is greater than their health. However, they are destroyed immediately.
+function BarrierLib:set_is_aura(is_aura)
+  self._is_aura = is_aura
+  if is_aura == true then
+    self:set_default_defense_aura()
+  else
+    self:set_default_defense_barrier()
+  end
+end
+
+function BarrierLib:is_aura()
+  return self._is_aura or false
+end
+
+function BarrierLib:set_weakness_element(element_list)
+  self._elemental_weakness = element_list
+end
+
+function BarrierLib:get_weakness_element()
+  return self._elemental_weakness
+end
+
+---@param flags table
+function BarrierLib:set_hit_flag_weakness(flags)
+  self._hit_flag_weakness = flags
+end
+
+function BarrierLib:get_hit_flag_weakness()
+  return self._hit_flag_weakness
+end
+
+function BarrierLib:shared_weakness_check(defense, hit_props)
+  if self:check_weakness_hit(hit_props) == true then
+    self._is_weakness_hit = true
+    self:do_destruction_removal()
+    return true
+  end
+
+  if hit_props.flags & Hit.Drain ~= 0 then return true end
+
+  return false
+end
+
+function BarrierLib:set_default_defense_barrier()
+  self._defense_rule.defense_func = function(defense, attacker, defender, hit_props)
+    if self:shared_weakness_check(defense, hit_props) == true then return end
+
+    self._health = math.max(0, self._health - hit_props.damage)
+
+    if self._health == 0 then
+      if self._regenerate_after_wait == true then
+        self:do_destruction_removal()
+      else
+        self:do_shared_removal()
+      end
+    end
+
+    defense:block_damage()
+  end
+end
+
+function BarrierLib:set_default_defense_aura()
+  self._defense_rule.defense_func = function(defense, attacker, defender, hit_props)
+    if self:shared_weakness_check(defense, hit_props) == true then return end
+
+    if hit_props.damage >= self._health then
+      self._health = 0
+      self:do_shared_removal()
+    end
+
+    defense:block_damage()
+  end
+end
+
+---@param hit_props HitProperties
+function BarrierLib:check_weakness_hit(hit_props)
+  if self._hit_flag_weakness ~= nil then
+    local i = 1
+    local found = false
+
+    while i < #self._hit_flag_weakness and found == false do
+      local flag = self._hit_flag_weakness[i]
+      if hit_props.flags & flag ~= 0 then
+        found = true
+      end
+      i = i + 1
+    end
+
+    if found == true then return true end
+  end
+
+  if self._elemental_weakness ~= nil then
+    local ii = 1
+    local found = false
+
+    while ii < #self._elemental_weakness and found == false do
+      local elem = self._elemental_weakness[ii]
+      if hit_props.element == elem or hit_props.secondary_element == elem then found = true end
+      ii = ii + 1
+    end
+
+    if found == true then return true end
+  end
+
+  return false
+end
+
+---@param self BarrierLib
+function BarrierLib:setup(owner)
+  self:set_entity_as_owner(owner)
+
+  self._defense_rule = DefenseRule.new(DefensePriority.Barrier, DefenseOrder.Always)
+
+  self._is_fade = false
+  self._is_weakness_hit = false
+  if self._regenerate_after_wait == nil then self._regenerate_after_wait = false end
+end
+
+function BarrierLib:enable_regeneration_timer(is_enabled)
+  self._regenerate_after_wait = is_enabled
+  self._barrier_regeneration_timer = 0
+
+  self._regeneration_timer_component = self._owner:create_component(Lifetime.ActiveBattle)
+
+  self._regeneration_timer_component.on_update_func = function()
+    if self._health > 0 then return end
+    if self._regenerate_after_wait == false then return end
+
+    if self._is_draw_health == true then
+      self._number_root:hide()
+    end
+
+    if self._barrier_regeneration_timer >= self._regenerate_barrier_after then
+      self._barrier_node:reveal()
+
+      if self._is_draw_health == true then
+        self._number_root:reveal()
+      end
+
+      self:set_health(self:get_max_health())
+
+      if self._regeneration_audio ~= nil then
+        Resources.play_audio(self._regeneration_audio)
+      end
+
+      self._barrier_regeneration_timer = 0
+    end
+
+    self._barrier_regeneration_timer = self._barrier_regeneration_timer + 1
+  end
+end
+
+function BarrierLib:set_regeneration_timer(time)
+  self._regenerate_barrier_after = time
+  self:enable_regeneration_timer(true)
+end
+
+function BarrierLib:set_regeneration_audio(audio)
+  self._regeneration_audio = audio
+end
+
+function BarrierLib:setup_animation()
+  local texture = self:get_texture()
+
+  self._barrier_node = self._owner:create_node()
+  self._barrier_node:set_layer(3)
+  self._barrier_node:set_texture(texture)
+
+  local anim = self:get_animation_path()
+
+  self._barrier_animation = Animation.new(anim)
+
+  self._barrier_animation:set_state(self._active_state)
+  self._barrier_animation:set_playback(Playback.Loop)
+  self._barrier_animation:apply(self._barrier_node)
+
+  self._animation_component = self._owner:create_component(Lifetime.ActiveBattle)
+
+  if self._is_draw_health == true then
+    self._number_root = self._owner:create_node()
+    self._number_root:set_never_flip(true)
+
+    self._health_text = self._number_root:create_text_node(TextStyle.new("THICK"), tostring(self._health))
+    self._health_text:set_color(Color.new(0, 0, 0, 255))
+    self._health_text:set_offset(-2 * (#tostring(self._health)) - 1, 2)
+    self._health_text:set_layer(-3)
+
+    self._health_text._shadow = self._health_text:create_text_node(TextStyle.new("THICK"), tostring(self._health))
+    self._health_text._shadow:set_offset(-1, -1)
+  end
+
+  self._animation_component.on_update_func = function()
+    if self:check_needs_removal() == true then return end
+
+    if type(self._removal_timer) == "number" then
+      self._removal_timer = self._removal_timer - 1
+      if self._removal_timer == 0 then
+        self._is_fade = true
+        self:do_fade_removal()
+        return
+      end
+    end
+
+    self:check_needs_removal()
+
+    self._barrier_animation:update()
+    self._barrier_animation:apply(self._barrier_node)
+  end
+
+  self._destruction_handler_component = self._owner:create_component(Lifetime.Battle)
+
+  self._defense_rule.on_replace_func = function()
+    self:do_shared_removal()
+  end
+end
+
+function BarrierLib:do_shared_removal()
+  if self._regeneration_timer_component ~= nil then self._regeneration_timer_component:eject() end
+
+  if self._is_draw_health == true then
+    self._owner:sprite():remove_node(self._number_root)
+  end
+
+  self._animation_component:eject()
+  self._destruction_handler_component:eject()
+  self._owner:sprite():remove_node(self._barrier_node)
+  self._owner:remove_defense_rule(self._defense_rule)
+end
+
+function BarrierLib:do_destruction_removal()
+  if self._destruction_sound ~= nil then
+    Resources.play_audio(self._destruction_sound)
+  end
+
+  if self._destroyed_state ~= nil then
+    self._barrier_animation:set_state(self._destroyed_state)
+    self._barrier_animation:on_complete(function()
+      self._barrier_node:hide()
+      self._barrier_animation:set_state(self._active_state)
+      self._barrier_animation:set_playback(Playback.Loop)
+      self._barrier_animation:apply(self._barrier_node)
+    end)
+  end
+
+  if self._regenerate_after_wait == true then return end
+
+  self:do_shared_removal()
+end
+
+function BarrierLib:do_fade_removal()
+  if self._fade_state ~= nil then
+    self._barrier_animation:set_state(self._fade_state)
+    self._barrier_animation:on_complete(function()
+      self:do_shared_removal()
+    end)
+  else
+    self._fade_component = self._owner:create_component(Lifetime.Battle)
+
+    self._fade_component._timer = 0
+
+    self._fade_component.on_update_func = function(compself)
+      if compself._timer >= 40 then
+        self:do_shared_removal()
+        compself:eject()
+        return
+      end
+
+      if compself._timer % 5 == true then
+        self._barrier_node:set_visible(not self._barrier_node:visible())
+      end
+
+      compself._timer = compself._timer + 1
+    end
+  end
+end
+
+function BarrierLib:check_needs_removal()
+  if self._is_fade == true then return true end
+  if self._is_weakness_hit == true then return true end
+  if self._health == 0 and self._regenerate_after_wait == false then return true end
+
+  return self._defense_rule:replaced()
+end
+
+function BarrierLib:remove_barrier()
+  if self._is_fade == true then
+    self:do_fade_removal()
+  else
+    self:do_shared_removal()
+  end
+end
+
+function BarrierLib:add_to_owner(offset)
+  self:setup_animation()
+  self._owner:add_defense_rule(self._defense_rule)
+
+  if not self._barrier_node:visible() then self._barrier_node:reveal() end
+
+  if self._is_draw_health == true then
+    self._health_text:reveal()
+    self._health_text._shadow:reveal()
+  end
+
+  if offset ~= nil then
+    self:set_barrier_offset(offset)
+  end
+end
+
+function BarrierLib:set_entity_as_owner(entity)
+  self._owner = entity
+end
+
+function BarrierLib:get_owner()
+  return self._owner
+end
+
+function BarrierLib:set_barrier_offset(offset)
+  self._barrier_node:set_offset(offset.x, offset.y)
+end
+
+function BarrierLib:get_barrier_offset()
+  return self._barrier_node:offset()
+end
+
+function BarrierLib:get_defense_rule()
+  return self._defense_rule
+end
+
+---@return BarrierLib
+function BarrierLib.new_barrier(owner, health)
+  local barrier = {}
+
+  setmetatable(barrier, BarrierLib)
+
+  barrier:set_health(health)
+  barrier:set_max_health(health)
+  barrier:setup(owner)
+  barrier:set_is_aura(false)
+
+  return barrier
+end
+
+function BarrierLib.new_aura(owner, health)
+  local aura = {}
+
+  setmetatable(aura, BarrierLib)
+
+  aura:set_health(health)
+  aura:set_max_health(health)
+  aura:setup(owner)
+  aura:set_is_aura(true)
+
+  return aura
+end
+
+return BarrierLib
