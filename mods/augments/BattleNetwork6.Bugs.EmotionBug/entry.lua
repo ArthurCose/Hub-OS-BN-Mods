a6be5243c7e61c10c693e2863af742aebce5b479
Arthur Cosentino <arthurcose@gmail.com>
2025-12-06 17:18:33 -0500

    Pull in proper emotion bug

---
+++ mods/augments/BattleNetwork6.Bugs.EmotionBug/entry.lua
@@ -0,0 +1,66 @@
+---@param augment Augment
+function augment_init(augment)
+  local player = augment:owner()
+  local bug_aux_prop = AuxProp.new()
+      :require_interval(60)
+      :with_callback(function()
+        local chance = math.floor(math.random(1, 16))
+        local normal_emotion_name = "NORMAL"
+        local synchro_emotion_name = "SYNCHRO"
+        local anger_emotion_name = "ANGRY"
+        local tired_emotion_name = "TIRED"
+        local emotions = player:emotions()
+
+        for index, value in ipairs(emotions) do
+          if value == "DEFAULT" then normal_emotion_name = value end
+          if value == "FULL_SYNCHRO" then synchro_emotion_name = value end
+          if value == "ANGER" or value == "RAGE" then anger_emotion_name = value end
+          if value == "WORRIED" then tired_emotion_name = value end
+        end
+
+        local normal_emotion_table = {
+          { emotion = tired_emotion_name,   range = 14 },
+          { emotion = anger_emotion_name,   range = 15 },
+          { emotion = synchro_emotion_name, range = 16 }
+        }
+        local tired_emotion_table = {
+          { emotion = normal_emotion_name,  range = 14 },
+          { emotion = anger_emotion_name,   range = 15 },
+          { emotion = synchro_emotion_name, range = 16 },
+        }
+        local angry_emotion_table = {
+          { emotion = normal_emotion_name,  range = 7 },
+          { emotion = tired_emotion_name,   range = 15 },
+          { emotion = synchro_emotion_name, range = 16 },
+        }
+        local synchro_emotion_table = {
+          { emotion = normal_emotion_name, range = 7 },
+          { emotion = tired_emotion_name,  range = 15 },
+          { emotion = anger_emotion_name,  range = 16 },
+        }
+
+        local table_of_tables = {}
+        table_of_tables[normal_emotion_name] = normal_emotion_table
+        table_of_tables[tired_emotion_name] = tired_emotion_table
+        table_of_tables[anger_emotion_name] = angry_emotion_table
+        table_of_tables[synchro_emotion_name] = synchro_emotion_table
+
+        local current_emotion = player:emotion()
+        local chosen_table = table_of_tables[current_emotion]
+
+        if chosen_table == nil then return end
+
+        for index, value in ipairs(chosen_table) do
+          if chance <= value.range then
+            player:set_emotion(value.emotion)
+            break
+          end
+        end
+      end)
+
+  player:add_aux_prop(bug_aux_prop)
+
+  augment.on_delete_func = function()
+    player:remove_aux_prop(bug_aux_prop)
+  end
+end
