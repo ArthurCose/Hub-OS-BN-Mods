42b69d1132dde6944408b7afcd9bb50bef32f580
Arthur Cosentino <arthurcose@gmail.com>
2026-01-15 16:55:18 -0500

    Use new movement input func to on movement bugs to work with warpstep

--- mods/augments/BattleNetwork4.Bugs.BackwardMovement/entry.lua
+++ mods/augments/BattleNetwork4.Bugs.BackwardMovement/entry.lua
@@ -8,15 +8,13 @@ function augment_init(augment)
     owner:boost_augment(foreward_augment:id(), -foreward_augment:level())
   end
 
-  local component = owner:create_component(Lifetime.ActiveBattle)
   local frequency = 7
   local time = 0
 
-  component.on_update_func = function()
+  augment.movement_input_func = function()
     time = time + 1
 
     if time % frequency ~= 0 then return end
-    if owner:is_moving() or owner:has_actions() then return end
 
     -- avoid moving the player if they are fighting the movement
     if owner:input_has(Input.Held.Left) or
@@ -27,17 +25,12 @@ function augment_init(augment)
       return
     end
 
-    local tile = owner:get_tile(owner:facing_away(), 1)
-
-    if tile and owner:can_move_to(tile) then
-      owner:queue_default_player_movement(tile)
-    end
+    return owner:facing_away()
   end
 
   owner:boost_augment("BattleNetwork.Bugs.EmotionFlicker", 1)
 
   augment.on_delete_func = function()
     owner:boost_augment("BattleNetwork.Bugs.EmotionFlicker", -1)
-    component:eject()
   end
 end
